eb15d0c1abac8fb9ced6440e971d840dd2913b24
AinoMegumi <aimegu2014@gmail.com>
2019-06-23 19:26:25 +0900

    乱数エンジンを使えるように変更

--- DefeatMonsterAICreateBaseProject/ExternalAI.hpp
+++ DefeatMonsterAICreateBaseProject/ExternalAI.hpp
@@ -10,16 +10,16 @@ namespace Offline {
 #ifdef MAKE_PARTNER_AI
 	class ExternalPartnerAI {
 	public:
-		virtual SkillA command(const std::array<Parameter, 4>& CurrentParameter) noexcept = 0;
-		virtual size_t GetCureTarget() noexcept = 0;
-		size_t GetTarget() noexcept { return this->GetCureTarget(); }
+		virtual SkillA command(const std::array<Parameter, 4>& CurrentParameter, std::mt19937& RandEngine) noexcept = 0;
+		virtual size_t GetCureTarget(std::mt19937& RandEngine) noexcept = 0;
+		size_t GetTarget(std::mt19937& RandEngine) noexcept { return this->GetCureTarget(RandEngine); }
 	};
 #else
 	class ExternalEnemyAI {
 	public:
-		virtual SkillA command(const std::array<Parameter, 4>& CurrentParameter) noexcept = 0;
-		virtual size_t GetAttackTarget() noexcept = 0;
-		size_t GetTarget() noexcept { return this->GetAttackTarget(); }
+		virtual SkillA command(const std::array<Parameter, 4>& CurrentParameter, std::mt19937& RandEngine) noexcept = 0;
+		virtual size_t GetAttackTarget(std::mt19937& RandEngine) noexcept = 0;
+		size_t GetTarget(std::mt19937& RandEngine) noexcept { return this->GetAttackTarget(RandEngine); }
 	};
 #endif
 }
@@ -27,16 +27,16 @@ namespace Offline {
 namespace Online {
 #ifdef MAKE_PARTNER_AI
 	class ExternalPartnerAI {
-		virtual SkillA command(const std::array<Parameter, 5>& CurrentParameter) noexcept = 0;
-		virtual size_t GetCureTarget() noexcept = 0;
-		size_t GetTarget() noexcept { return this->GetCureTarget(); }
+		virtual SkillA command(const std::array<Parameter, 5>& CurrentParameter, std::mt19937& RandEngine) noexcept = 0;
+		virtual size_t GetCureTarget(std::mt19937& RandEngine) noexcept = 0;
+		size_t GetTarget(std::mt19937& RandEngine) noexcept { return this->GetCureTarget(RandEngine); }
 	};
 #else
 	class ExternalEnemyAI {
 	public:
-		virtual SkillA command(const std::array<Parameter, 5>& CurrentParameter) noexcept = 0;
-		virtual size_t GetAttackTarget() noexcept = 0;
-		size_t GetTarget() noexcept { return this->GetAttackTarget(); }
+		virtual SkillA command(const std::array<Parameter, 5>& CurrentParameter, std::mt19937& RandEngine) noexcept = 0;
+		virtual size_t GetAttackTarget(std::mt19937& RandEngine) noexcept = 0;
+		size_t GetTarget(std::mt19937& RandEngine) noexcept { return this->GetAttackTarget(RandEngine); }
 	};
 #endif
 }
